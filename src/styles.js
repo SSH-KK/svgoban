d20b1f91a1d8cc800cb267fa762e66cfefe33a3e
TUTOR03 <artemss20030906@gmail.com>
2021-04-30 20:55:33 +0300

    aaaaa

--- src/styles.js
+++ src/styles.js
@@ -8,7 +8,7 @@
 /**
  * Chosen constants for a top left radial gradient
  */
-var SV_GRAD = {
+ var SV_GRAD = {
     "cx" : "50%",
     "cy" : "45%",
     "r"  : "60%",
@@ -24,7 +24,19 @@ var SV_BW = {
     "white": {
 	"start" : "rgb(255,255,255)", 
 	"stop"  : "rgb(180,180,180)"
-    }
+    },
+    "red": {
+		"start" : "rgb(54,199,151,0.3715861344537815)", 
+		"stop"  : "rgb(32,231,193,1)"
+		},
+    "green": {
+			"start" : "rgb(156,237,25,0.3715861344537815)", 
+			"stop"  : "rgb(63,255,0,1)"
+		},
+    "blue": {
+			"start" : "rgb(25,177,237,0.3715861344537815)", 
+			"stop"  : "rgb(0,114,255,1)"
+		}
 };
 
 exports.defineRadialColors = function(color) {
@@ -40,6 +52,10 @@ exports.Themes = {
 	return `
 	    .wood { 
 		fill: #b4916c; 
+			}
+			.redMarker {
+		stroke: red !important;
+		fill: red !important;
 	    }
 	    .placeholder { 
 		stroke: black;
@@ -60,6 +76,24 @@ exports.Themes = {
 	    }
 	    .whitestone { 
 		fill: url(#whitegrad);
+	    }
+	    .redstone { 
+				fill: url(#redgrad) !important;
+				stroke: none !important;
+				opacity: 0.5;
+				r: 4%;
+			}
+			.greenstone { 
+				fill: url(#greengrad) !important;
+				stroke: none !important;
+				opacity: 0.5;
+				r: 4%;
+			}
+			.bluestone { 
+				fill: url(#bluegrad) !important;
+				stroke: none !important;
+				opacity: 0.5;
+				r: 4%;
 	    }
 	    .onblack {
 		stroke: white;
@@ -165,6 +199,69 @@ exports.Themes = {
 	    font-size: 1.1em; 
 	}
 	`;
-    }
+    },
+	'new_night': function(){
+		return `
+	    .wood { 
+		fill: #212529; 
+	    }
+	    .placeholder { 
+		fill: black;
+		stroke: black;
+		opacity: 0 
+	    }
+	    .black .placeholder:hover { 
+		fill: black;
+		stroke: black;
+		opacity: 0.2 
+	    }
+	    .white .placeholder:hover { 
+		fill: white;
+		stroke: black;
+		opacity: 0.2 
+	    }
+	    .blackstone { 
+		fill: #292929; 
+		stroke: white;
+	    }
+	    .whitestone { 
+		fill: #F0F0F0;
+		stroke: ##3F3F3F;
+	    }
+	    .onblack {
+		stroke: white;
+		fill: none;
+	    }
+	    .onwhite {
+		stroke: black;
+		fill: none;
+	    }
+		.star_points {
+			fill: #FFFFFF;
+		}
+		.last_move_marker {
+			fill: #20E7C1;
+			stroke: none;
+		}
+		.on_green_stroke{
+			fill: none;
+			stroke: #20E7C1;
+		}
+		.redstone {
+			fill: url(#redgrad) !important;
+			stroke: none !important;
+			opacity: 0.5;
+			r: 4%;
+		}
+	line, path {
+	    stroke: #FFFFFF; 
+	}
+	text { 
+	    font-family: sans-serif; 
+	    font-size: 1.1em; 
+		fill: #FFFFFF;
+	}
+	`
+	}
 }
 
